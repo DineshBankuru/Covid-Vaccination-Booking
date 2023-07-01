305c7957931c1c33a6f5949f867e7f3888451a91
DineshBankuru <108983416+DineshBankuru@users.noreply.github.com>
2023-07-01 15:53:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(passport.session());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-mongoose.connect("mongodb+srv://dineshbankuru2004:"+process.env.MONGO+"@cluster0.5ptp29j.mongodb.net/covid" , {useNewUrlParser : true});
+mongoose.connect("mongodb+srv://dineshbankuru2004:"+process.env.MONGO+"@cluster0.f7z8mvo.mongodb.net/covid" , {useNewUrlParser : true});
 
 
 
